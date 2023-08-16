4d89c3d44e6e858c8c4133d3fa7b1344f80a7364
stanliwise <31096369+stanliwise@users.noreply.github.com>
2023-08-16 20:03:59 +0100

    added and publish config and routes

--- src/ParkwayWalletServiceProvider.php
+++ src/ParkwayWalletServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Parkway\Wallet\Sdk;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Parkway\Wallet\Sdk\Http\Middleware\ValidateRequestSignature;
 
 class ParkwayWalletServiceProvider extends ServiceProvider
 {
@@ -13,15 +15,35 @@ class ParkwayWalletServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->publishes([
+            __DIR__.'/../config/config.php' => config_path('pwsdk.php'),
+        ], 'pwsdk-config');
     }
-
 
     /**
      * Register the console commands for the package.
      *
      * @return void
      */
-    protected function registerCommands()
+    public function register(): void
     {
+
+        #merge configuration
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'pwsdk');
+
+        #add middleware
+        app()->make('router')->aliasMiddleware('parkway.verify-signature', ValidateRequestSignature::class);
+
+        #register route
+        $this->registerRoutes();
+    }
+
+    public function registerRoutes()
+    {
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(function () {
+                $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
+            });
     }
 }
