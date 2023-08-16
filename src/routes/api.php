acee8ef4213f992cfd2be811f7af09802fa84e80
stanliwise <31096369+stanliwise@users.noreply.github.com>
2023-08-16 20:03:59 +0100

    added and publish config and routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('signature.validate')->prefix('transact')->group(function(){
-    ///Route::post('walletToWallet', [])
+Route::middleware('parkway.verify-signature')->prefix('transact')->group(function(){
+    Route::post('walletToWallet', function(){
+        logger('worked well');
+    });
 });
