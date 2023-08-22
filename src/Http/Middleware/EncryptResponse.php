661ae546c2d6d4b870baa3b8bf9cf89af1158764
stanliwise <31096369+stanliwise@users.noreply.github.com>
2023-08-22 18:30:51 +0100

    push addition of wallet payload encryption

---
+++ src/Http/Middleware/EncryptResponse.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Parkway\Wallet\Sdk\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+use Spatie\Crypto\Rsa\PrivateKey;
+use Spatie\Crypto\Rsa\PublicKey;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class EncryptResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     * @return JsonResponse|RedirectResponse|mixed|Response
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // Get the response from the next middleware or controller
+        $response = $next($request);
+
+        try {
+            //code...
+            // Encrypt the JSON data
+            $encryptedData = PrivateKey::fromFile(storage_path('private.key'))->sign($response->getContent() ?? '');
+
+            // Set the encrypted data in the "PW-header" header
+            $response->headers->set('PW-header', $encryptedData);
+        } catch (\Throwable $th) {
+            abort(Response::HTTP_INTERNAL_SERVER_ERROR, 'An unknown server error occured');
+        }
+
+        return $response;
+    }
+
+    protected  function base64ToUrlEncode($data)
+    {
+        // Replace characters that are not URL-safe
+        $base64Url = strtr($data, '+/', '-_');
+
+        // Remove padding characters
+        $base64Url = rtrim($base64Url, '=');
+
+        return $base64Url;
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Parkway\Wallet\Sdk\Http\Controllers\TransactionController;
 
-Route::middleware('pwsdk.verify-signature')->prefix('transact')->group(function(){
-    Route::post('walletToWallet', function(){
-        logger('worked well');
-    });
+Route::middleware(['pwsdk.verify-signature', 'pwsdk.encryt-response'])->prefix('transact')->group(function () {
+    Route::post('{walletNumber}/transferToWallet', [TransactionController::class, 'walletToWallet']);
+    Route::post('{walletNumber}/transferToBank', [TransactionController::class, 'walletToBank']);
 });
