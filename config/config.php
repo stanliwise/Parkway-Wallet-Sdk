5a032a8870588bc9e3546721307983c6af08813e
stanliwise <31096369+stanliwise@users.noreply.github.com>
2023-08-23 20:26:08 +0100

    added rsa keys

--- config/config.php
+++ config/config.php
@@ -1,16 +1,12 @@
 <?php
 
 return [
-    'public_key' => "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA9roDQngVWY9ZoEvBXPDb
-    Xpvd8j2c5cIsrm2RdL2rmQynaSygHpph7yAegecRpanky0BgB4HdZGLBucextNH5
-    N1nXwAFRDSJuDkep2TSgq6/nVrnzfjUo1O0KhTZ3q+xbLYbVE9IyKV2X+w3O+2H/
-    OkoX+SNl/Ex7j70AYSdkAP2rRlBFQ3nJyqM5YPXqbinqoXNtB1It07nm6lwt4Fua
-    JI/hQ0tKeDrLkdbVkoJyltuZx5UtynfezDVSfjsUrHkURAaxKxLQkKZZtHDyodJK
-    IEEY8sCIcBF7Fl0KuMkU2K5VrDIml2t2G/cxrIAKCINc1FYHmoW8bn+vy1CnOrAR
-    DO5x15IvAFSghFgPCe8tGVXXexHwqK2oMiv6A2habwnHdbL9YJMt5lG/HtfxFk+4
-    tmLjCtybziP5dK1RVW9ubUzhezKyaM1GU87fkvV8Mi0taC3tkXxn7vYjuKWtbfWU
-    1hT/fgC6g9pI/4wKT2Bum+KL07hEmd/vhsXKJTJ99UtsAtzt54WLOotv3sAUCHLH
-    796Zii7WtnU3O3hRgsdWN/AbkCvb1ycAJCbquXyJtZjEJgB1y9t3QymNyw5JdMd0
-    kZwKax/zO86e5HfI1/Qo0P+7uOXgpDTSqUqj6QuyY+z24IhXf5wkwo20jcpusnSK
-    BWoT9uq9Ggi4/DUJkrnpfX8CAwEAAQ=="
+    'parkway-wallet' => [
+        'publicKeyPath' => env('PARKWAY_PUBLIC_KEY_PATH', storage_path('pwsdk/public.key')),
+        'baseUrl' => env('PARKWAY_WALLET_BASE_URL', 'https://wallet.parkway.ng')
+    ],
+
+    'publicKeyPath' => env('RSA_PUBLIC_KEY_PATH', storage_path('rsaKeys/public.key')),
+
+    'privateKeyPath' => env('RSA_PRIVATE_KEY_PATH', storage_path('rsaKeys/private.key')),
 ];
