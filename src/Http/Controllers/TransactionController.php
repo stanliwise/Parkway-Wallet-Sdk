08f736d978b21452892092b6c7a0d7ce28a06a20
stanliwise <31096369+stanliwise@users.noreply.github.com>
2023-11-10 22:39:38 +0100

    just push something

--- src/Http/Controllers/TransactionController.php
+++ src/Http/Controllers/TransactionController.php
@@ -107,6 +107,7 @@ class TransactionController
             return response()->json($response);
         } catch (\Throwable $th) {
             logger($th);
+            
             return [
                 'code' => '00988',
                 'desc' => 'An Unknown error occured',
